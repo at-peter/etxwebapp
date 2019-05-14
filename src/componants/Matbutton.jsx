84ab3f17c1d10b75fe226404374822129accc3db
Peter Atrazhev <atrazhev@ualberta.ca>
2019-05-14 16:03:07 -0600

    Cleaned out my react app code and inserted material-ui code

---
+++ src/componants/Matbutton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const Mybutton = () => (
+  <Button variant="contained" color="primary">
+    Play games
+  </Button>
+);
+
+export default  Mybutton;
