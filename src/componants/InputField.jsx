f87dc7519a9f979548c30af3b221809489ed8471
peter <atrazhev@ualberta.ca>
2019-05-15 09:14:39 -0600

    starting to make input div componants

---
+++ src/componants/InputField.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class InputField extends React.Componant{
+
+    constructor(props){
+        super(props);
+        // state is set here 
+        this.state = {
+            buttontoggle : 0,
+            buyfield : 0, 
+            askfield : 0
+        };
+    }
+    
+    render(
+        // this needs to render all three componants: submit button, ask and buy fields. 
+    );
+
+
+} 
